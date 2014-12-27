4e41cdc3ff0f23b064bf34ec500dea55c5647995
Thomas S.  Knutsen <la3pna@gmail.com>
2014-12-27 13:40:01 +0100

    serial, delay and millis sketches

---
+++ 1_Intro/serial.ino
@@ -0,0 +1,16 @@
+/*
+ Example code for the serial transmission. 
+ by Thomas S. Knutsen 27 dec 2014.
+ This example code is in the public domain.
+ */
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  Serial.print("hello world")
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+}
