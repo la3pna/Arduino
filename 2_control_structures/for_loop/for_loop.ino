e00ead36a52dcb1f95095fd605aceb65e461da85
Thomas S.  Knutsen <la3pna@gmail.com>
2015-01-09 20:16:38 +0100

    control structures - for

---
+++ 2_control_structures/for_loop/for_loop.ino
@@ -0,0 +1,19 @@
+//
+// program showing the for loop
+//
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  for(int i=0; i>=10;i=i+1){
+    
+    Serial.println(i);
+    }
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
