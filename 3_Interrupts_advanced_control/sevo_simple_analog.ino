64babb2a09cfb15034cc6699a561c40bd52ce841
Thomas S.  Knutsen <la3pna@gmail.com>
2015-02-11 21:59:29 +0100

    Update sevo_simple_analog.ino

--- 3_Interrupts_advanced_control/sevo_simple_analog.ino
+++ 3_Interrupts_advanced_control/sevo_simple_analog.ino
@@ -16,7 +16,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
-  val = map(val, 0, 1023, 0, 180); 
+  val = map(val, 0, 1023, 1000, 2000); // maps the pot value into 1-2ms range.
 digitalWrite(servoPin, HIGH);
 delayMicroseconds(val);
 digitalWrite(servoPin,LOW);
